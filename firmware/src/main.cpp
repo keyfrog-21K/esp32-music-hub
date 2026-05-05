adedef86528d120a49fba056d7a665df43232c9b
keyfrog-21K <p@keyfrog.kr>
2026-05-05 13:07:50 +0900

    feat: 한글 지원 및 제목 줄바꿈 적용

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,41 +3,91 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <TFT_eSPI.h>
+#include <U8g2_for_TFT_eSPI.h>
 #include "env.h"
 
-// CYD 백라이트 핀 번호
 #define TFT_BL 21
 
 TFT_eSPI tft = TFT_eSPI();
+U8g2_for_TFT_eSPI u8f;
+
+// 줄바꿈 처리를 위한 함수 (간이 구현)
+void drawWrappedText(const char* text, int x, int y, int maxWidth, int lineHeight) {
+    String str = String(text);
+    int startIdx = 0;
+    int currentY = y;
+    
+    while (startIdx < str.length()) {
+        int charCount = 0;
+        int currentWidth = 0;
+        
+        while (startIdx + charCount < str.length()) {
+            unsigned char c = (unsigned char)str[startIdx + charCount];
+            int charWidth;
+            int step;
+            
+            if (c < 128) { // 영문/숫자
+                charWidth = 9;
+                step = 1;
+            } else if ((c & 0xE0) == 0xC0) {
+                charWidth = 16;
+                step = 2;
+            } else if ((c & 0xF0) == 0xE0) {
+                charWidth = 16;
+                step = 3;
+            } else {
+                charWidth = 16;
+                step = 4;
+            }
+            
+            if (currentWidth + charWidth > maxWidth) break;
+            
+            currentWidth += charWidth;
+            charCount += step;
+        }
+        
+        if (charCount == 0) break;
+        
+        u8f.setCursor(x, currentY);
+        u8f.print(str.substring(startIdx, startIdx + charCount));
+        
+        startIdx += charCount;
+        currentY += lineHeight;
+        
+        if (currentY > 230) break; 
+    }
+}
 
 void setup() {
     Serial.begin(115200);
-
-    // 백라이트 강제 켜기
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, HIGH); 
 
     tft.init();
     tft.setRotation(1);
-    
-    // 화면 테스트를 위해 처음에 빨간색으로 채움
-    tft.fillScreen(TFT_RED); 
-    delay(500);
     tft.fillScreen(TFT_BLACK);
 
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.drawString("Connecting to WiFi...", 10, 10, 2);
+    u8f.begin(tft); 
+    u8f.setFontMode(1); 
+    u8f.setFontDirection(0);
 
-    Serial.println("Connecting to WiFi...");
+    // korean2 폰트 적용
+    u8f.setFont(u8g2_font_unifont_t_korean2); 
+    u8f.setForegroundColor(TFT_WHITE);
+    
+    u8f.setCursor(10, 40);
+    u8f.print("와이파이 연결 중...");
+
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
     
-    Serial.println("\nWiFi Connected!");
     tft.fillScreen(TFT_BLACK);
-    tft.drawString("WiFi Connected!", 10, 10, 2);
+    u8f.setCursor(10, 40);
+    u8f.print("연결 성공!");
+    delay(1000);
 }
 
 void loop() {
@@ -54,18 +104,18 @@ void loop() {
             const char* title = doc["title"];
             const char* artist = doc["artist"];
 
-            Serial.printf("Update Display: %s - %s\n", title, artist);
-
             tft.fillScreen(TFT_BLACK);
-            tft.setTextColor(TFT_WHITE, TFT_BLACK);
-            tft.drawString(title, 10, 60, 4); 
             
-            tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-            tft.drawString(artist, 10, 100, 2);
-        } else {
-            Serial.printf("HTTP Error: %d\n", httpCode);
+            // 노래 제목 (상단)
+            u8f.setFont(u8g2_font_unifont_t_korean2);
+            u8f.setForegroundColor(TFT_WHITE);
+            drawWrappedText(title, 10, 50, 300, 30);
+            
+            // 아티스트 (하단 노란색, 줄바꿈 적용)
+            u8f.setForegroundColor(TFT_YELLOW);
+            drawWrappedText(artist, 10, 180, 300, 25);
         }
         http.end();
     }
-    delay(2000);
+    delay(5000);
 }
