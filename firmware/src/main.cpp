ec8888d3d3e7edeca6da07c40991b59926365d75
keyfrog-21K <p@keyfrog.kr>
2026-05-05 12:40:30 +0900

    feat: 음악 정보 표시 기능 구현

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -5,20 +5,37 @@
 #include <TFT_eSPI.h>
 #include "env.h"
 
+// CYD 백라이트 핀 번호
+#define TFT_BL 21
+
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
     Serial.begin(115200);
+
+    // 백라이트 강제 켜기
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH); 
+
     tft.init();
     tft.setRotation(1);
+    
+    // 화면 테스트를 위해 처음에 빨간색으로 채움
+    tft.fillScreen(TFT_RED); 
+    delay(500);
     tft.fillScreen(TFT_BLACK);
+
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
     tft.drawString("Connecting to WiFi...", 10, 10, 2);
 
+    Serial.println("Connecting to WiFi...");
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
+    
+    Serial.println("\nWiFi Connected!");
     tft.fillScreen(TFT_BLACK);
     tft.drawString("WiFi Connected!", 10, 10, 2);
 }
@@ -37,11 +54,18 @@ void loop() {
             const char* title = doc["title"];
             const char* artist = doc["artist"];
 
+            Serial.printf("Update Display: %s - %s\n", title, artist);
+
             tft.fillScreen(TFT_BLACK);
-            tft.drawString(title, 10, 40, 4);
-            tft.drawString(artist, 10, 80, 2);
+            tft.setTextColor(TFT_WHITE, TFT_BLACK);
+            tft.drawString(title, 10, 60, 4); 
+            
+            tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+            tft.drawString(artist, 10, 100, 2);
+        } else {
+            Serial.printf("HTTP Error: %d\n", httpCode);
         }
         http.end();
     }
-    delay(5000); // 5초마다 업데이트
+    delay(5000);
 }
